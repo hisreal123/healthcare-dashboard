12aedf1ee0deeeef70548821d306273d32bb17bb
hisreal123 <omosgoerge@gmail.com>
2023-05-14 18:40:53 -0400

    getting intensive

---
+++ src/components/Misc/CreditCard.tsx
@@ -0,0 +1,103 @@
+import {Input} from "reactstrap";
+import {useState} from "react";
+
+export const CreditCard = () => {
+
+	const [cardOwner, setCardOwner] = useState('');
+	const [numberOnCard, setNumberOnCard] = useState<number>(0);
+	const [expDate, setExpDate] = useState<number>();
+	const [cvv, setCvv] = useState<number>();
+
+	const allCheck = false;
+	const alertHelp = () =>{
+		alert(" How can i help you !!!!");
+	};
+
+	//Input actions
+	const handleCardOwner = (e:any) =>{
+		setCardOwner(e.target.value)
+	}
+
+	const handleCardNumber = (e:any) =>{
+		setNumberOnCard(e.target.value)
+		console.log(numberOnCard)
+	}
+	const handleExpire = (e:any) =>{
+		setExpDate(e.target.value)
+		console.log(expDate)
+	}
+
+	const handleCvv = (e:any) =>{
+		setCvv(e.target.value)
+		console.log(cvv)
+	}
+
+
+
+	const handleSubmit = (e:any ) => {
+		e.peventDefault();
+
+	};
+
+	return (
+		<section className="">
+			<div className="flex justify-between ">
+				<h1 className="font-bold text-gray-800 ">Credit Card information</h1>
+				<span className="rounded-full border-none bg-gray-300/80 h-5 w-5 text-sm text-white text-center hover:bg-gray-300 cursor-pointer "
+					  onClick={alertHelp}
+				>?</span>
+			</div>
+
+			<form className="flex  flex-col space-y-5 mt-5"
+
+			onSubmit={handleSubmit}>
+			<Input
+				id="cardOwner"
+				name="cardOwner"
+				placeholder="Name of the Card Owner"
+				type="text"
+				value={cardOwner}
+				onChange={handleCardOwner}
+				required
+			/>
+				<Input
+				id="numberOnCard"
+				name="numberOnCard"
+				placeholder="Number on the Card"
+				type="number"
+				value={numberOnCard}
+				onChange={handleCardNumber}
+				required
+			/>
+				<Input
+				id="expDate"
+				name="expDate"
+				placeholder="Expiration Date "
+				type="number"
+				value={expDate}
+				onChange={handleExpire}
+				required
+			/>
+				<Input
+				id="cvv"
+				name="cvv"
+				placeholder="Security Code/CVV"
+				type="number"
+				value={cvv}
+				maxLength={3}
+				onChange={handleCvv}
+				required
+			/>
+
+				<button
+					type="submit"
+					className={`${!allCheck ? " bg-gray-200" : "bg-[#5282FB] hover:bg-[#0e46d3] "} disabled:cursor-not-allowed  mt-4 text-white font-light text-lg  w-full relative py-2 rounded-sm `}
+					disabled={!allCheck}
+				>Submit
+				</button>
+			</form>
+
+
+		</section>
+	);
+};
--- src/views/Payment.tsx
+++ src/views/Payment.tsx
@@ -1,9 +1,88 @@
-// import React from 'react'
+// import React from 'react
+// import { Suspense} from "react";
+// import {CreditCard} from "../components/Misc/CreditCard";
+import {Form, FormGroup, Input, Label} from "reactstrap";
 
-export default function Payment() {
+// @ts-ignore
+
+
+type tTypes = {
+    toggle: boolean,
+    image : string,
+    title : string,
+    caption: string
+}
+
+const PaymentOption: tTypes[]  = [
+    {
+        toggle: true,
+        image : '',
+        title : 'Credit Card',
+        caption: 'safe money transfer using your bank account Visa, Mastro'
+    },
+    {
+        toggle: true,
+        image : '',
+        title : 'PayPal',
+        caption: 'Credit card needed, safe payment online, PayPal account is necessary '
+    },
+]
+
+
+
+
+export default function Payment() : JSX.Element {
+    // @ts-ignore
     return (
         <>
-            <h1>Payment</h1>
+            <section className='lg:w-[90%] lg:mx-auto '>
+
+            <main className='py-3 grid lg:grid-cols-2 gap-5 '>
+                <div className='left '>
+                    <div className=''>
+                        <h1 className='text-gray-700/80 font-bold  text-xl '> Chose your payment method</h1>
+                        <p className='text-gray-400/80 text-xs '>Your payment information are secure and no sharing with third parties </p>
+                    </div>
+
+                    {/* choose wisely */}
+                    <div className='grid grid-cols-2 gap-5 mt-4'>
+                        {PaymentOption.map((pType, index) => {
+                            return(
+                                <div className=' relative border-2 p-4 hover:bg-blue-200 hover:border-blue-500 rounded-lg' key={index || pType.title}>
+                                    <p>{pType.image}</p>
+                                    <h2 className=' relativetitle text-lg lg:text-xl text-gray-800/70 mb-2'>{pType.title}</h2>
+                                    <span className='relative caption block text-xs text-gray-500 '>{pType.caption}</span>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className='right hidden lg:block'>
+                    <h1> Right</h1>
+                </div>
+                <Form>
+                    <FormGroup
+                        check
+                        inline
+                    >
+                        <Input type="checkbox" />
+                        <Label check>
+                            Some input
+                        </Label>
+                    </FormGroup>
+                    <FormGroup
+                        check
+                        inline
+                    >
+                        <Input type="checkbox" />
+                        <Label check>
+                            Some other input
+                        </Label>
+                    </FormGroup>
+                </Form>
+            </main>
+            </section>
         </>
     )
 }
